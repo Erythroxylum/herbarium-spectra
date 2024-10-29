6488f4cbd4eb32cf1e9fc8634e35d7147571be4a
Guzmán Q. <aguzman@fas.harvard.edu>
2024-10-29 16:18:18 -0400

    Implementing ConfusionMatrices

--- auxiliary/model_tune_plsda.R
+++ auxiliary/model_tune_plsda.R
@@ -2,109 +2,63 @@
 # Function for tune the optimal number of components of the PLSR models
 
 model_tune_plsda <- function(meta,
-                       split,
-                       segments,
-                       rank,
-                       spectra,
-                       ncomp_max,
-                       threads) {
-
+                             split,
+                             segments,
+                             species,
+                             spectra,
+                             ncomp_max,
+                             threads) {
+  
+  
   # Data for training
   meta_split <- meta[split,]
-  traits_split <- traits[split,]
-  rank_split <- rank[split,] # [split,5] for testing
+  species_split <- as.factor(species[split])
   spectra_split <- spectra[split,]
-
-  # combine taxon meta with spectra, then rename the taxon level to test as "taxon"
-  #frame <- cbind(traits_split, spectra_split)
-  frame <- cbind(rank_split, spectra_split)
-  ###### should the other levels of rank be removed?
-
+  
+  # Combine taxa with spectra
+  frame <- cbind(species_split, spectra_split)
+  colnames(frame)[1] <- "species"
+  
+  
   # Application loop
   tune <- function(X,
                    split,
                    segments,
                    frame,
-                   ncomp_max=30) {
-
+                   meta,
+                   ncomp_max) {
+    
     # Get samples from segments to train
     get_segments <- split %in% segments[[X]]
+    meta_split <- meta_split[get_segments,]
     sub_frame <- frame[get_segments,]
-
-    # Perform model
-    #ml_model <- plsr(rank ~ .,
-     #                data = sub_frame,
-      #               validation = "CV",
-       #              ncomp = ncomp_max,
-        #             center = TRUE,
-         #            method = "oscorespls",
-          #           maxit = 10000)
-
-    ctrl <- trainControl(method = "cv",
-                         #repeats = 10,
-                         number = 10,
-                         summaryFunction = multiClassSummary,
-                         #sampling = "none"
-                         classProbs = TRUE
-                         )
-
-    pls_model <- train(spShort ~ .,
-                       data = sub_frame,
-                       method = "pls",
-                       trControl = ctrl,
-                       preProcess = c("center", "scale"),
-                       tuneLength = 30,
-                       metric = 'ROC')
-
+    
+    # Model control
+    ctrl <- trainControl(index = data_folds(meta_split$species, k = 10))
+    
+    model <- train(species ~ .,
+                   data = sub_frame,
+                   method = "pls",
+                   trControl = ctrl,
+                   preProcess = c("center"),
+                   tuneGrid = expand.grid(ncomp = 1:ncomp_max),
+                   metric = "Accuracy",
+                   maxit = 10000)
+    
     # Performance
-    predictions <- predict(pls_model, newdata = as.matrix(sub_frame), type = "prob")
-
-    RMSEP_train <- postResample(predict(pls_model$bestTune, as.matrix(spectra_split)), sub_frame$spShort)
-    RMSEP_CV <- ml_model$resample$RMSE
-    R2_train <- cor(predict(ml_model, sub_frame), sub_frame$rank)^2  # R^2 necessary?
-
-    #RMSEP_train <- pls::RMSEP(object = ml_model,
-     #                         estimate = "train")$val[,,]
-
-    #RMSEP_CV <- pls::RMSEP(object = ml_model,
-     #                      estimate = "CV")$val[,,]
-
-    #R2_train <- pls::R2(object = ml_model,
-     #                   estimate = "train")$val[,,]
-
-    #R2_CV <- pls::R2(object = ml_model,
-     #                estimate = "CV")$val[,,]
-
-    #PRESS <- c(Intercept = 0, ml_model$validation$PRESS[1,])
-
-    # Collect results
     results <- data.table(iteration = X,
                           nsamples = nrow(sub_frame),
-                          metric = c("RMSEP", "R2"),
-                          estimate = c("train", "train"))
-
-    #results <- data.table(iteration = X,
-     #                     nsamples = nrow(sub_frame),
-      #                    metric = c("RMSEP", "RMSEP", "R2", "R2", "PRESS"),
-       #                   estimate = c("train", "CV", "train", "CV", "PRESS"))
-
-
-    metrics <- data.table(RMSEP = RMSEP_train[1], R2 = R2_train)
-    results <- cbind(results, metrics)
-
-    #metrics <- rbind(RMSEP_train, RMSEP_CV, R2_train, R2_CV, PRESS)
-    #results <- cbind(results, metrics)
-
+                          metric = colnames(model$results)[-1])
+    
+    results <- cbind(results, t(model$results)[-1,])
+    
     # Clean memory
-    rm(list = c("get_segments", "sub_frame", "ml_model", "metrics"))
-    #rm(list = c("get_segments", "sub_frame", "ml_model",
-     #           "RMSEP_train", "RMSEP_CV", "R2_train", "R2_CV",
-      #          "PRESS", "metrics"))
-
+    rm(list = c("get_segments", "sub_frame", "model"))
+    
     return(results)
-
+    
   }
-
+  
   # Parallel processing
   complete <- pbmclapply(X = 1:length(segments),
                          FUN = tune,
@@ -117,13 +71,13 @@ model_tune_plsda <- function(meta,
                          mc.cores = threads,
                          mc.cleanup = TRUE,
                          mc.allow.recursive = TRUE)
-
+  
   frame <- data.table()
-
+  
   for(i in 1:length(complete)) {
     frame <- rbind(frame, complete[[i]], fill = TRUE)
   }
-
+  
   return(frame)
-
+  
 }
