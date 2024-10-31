dba7a66423d19aaac6fd0994b4921f6f8960444c
Dawson White <dawsonwhite@Dawsons-MacBook-Pro-2239.local>
2024-10-31 15:15:52 -0400

    added anti_split as input for species_split performance testing

--- R/leaf_plsda.R
+++ R/leaf_plsda.R
@@ -39,6 +39,7 @@ source("auxiliary/confusionMatrices.R")
 # Select the root_folder to read data and export results
 
 root_path <- "C:/Users/jog4076/Downloads"
+root_path <- getwd()
 
 #'------------------------------------------------------------------------------
 #' @Read-Information
@@ -46,7 +47,7 @@ root_path <- "C:/Users/jog4076/Downloads"
 
 # Select the file of interest
 frame <- fread(paste0(root_path,
-                      "/fullDataHUH2024_sp25leaf636_noResample_400-2300.csv")) 
+                      "../fullDataHUH2024_sp25leaf636_noResample_400-2300.csv")) 
 frame <- frame[!is.na(leafKg_m2),]
 
 #-------------------------------------------------------------------------------
@@ -81,7 +82,7 @@ saveRDS(split, paste0(root_path, "/classification_split.rds"))
 #-------------------------------------------------------------------------------
 
 # Select a spectral measurement per specimen
-iterations <- 3 # 1000
+iterations <- 5 # 1000
 segments <- pbmclapply(X = 1:iterations,
                        FUN = data_segments,
                        meta = meta,
@@ -108,17 +109,32 @@ opt_models <- model_tune_plsda(meta = meta,
                                species = species,
                                spectra = spectra,
                                ncomp_max = ncomp_max,
-                               threads = 1) # If windows = 1, mac 2 same as 6
+                               threads = 2) # If windows = 1, mac 2 same as 6
 
+# Plot Accuracy
+pdf("../herbarium_spectra_results/ncomp_accuracy.pdf", width = 4, height = 3) 
 # Select the optimal based on the accuracy
 plot(x = 1:ncomp_max,
      y = colMeans(opt_models[metric == "Accuracy", 4:ncol(opt_models)]),
      xlab = "Number of components",
-     ylab = "PRESS")
+     ylab = "Accuracy") # was PRESS
+# Calculate mean accuracy for each component
+mean_accuracy <- colMeans(opt_models[metric == "Accuracy", 4:ncol(opt_models)])
+# Identify the component with the highest mean accuracy
+ncomp <- which.max(mean_accuracy)
+max_accuracy <- mean_accuracy[ncomp]
+# Add a point and annotate the highest accuracy point
+points(ncomp, max_accuracy, col = "red", pch = 19, cex = 1.2)
+text(ncomp, max_accuracy, labels = paste("Max Acc.:", round(max_accuracy, 2)),
+     pos = 1, offset = 2, col = "red")
+text(ncomp, max_accuracy, labels = paste("N comp.:", ncomp),
+     pos = 1, offset = 3, col = "red")
+# Close the PDF device to save the file
+dev.off()
 
 # Manual selection
-ncomp <- 39
-abline(v = ncomp, col = "red")
+#ncomp <- 35
+#abline(v = ncomp, col = "red")
 
 # Export for record and figures
 fwrite(opt_models, paste0(root_path, "/opt_comp_models.csv"))
@@ -134,7 +150,7 @@ models_plsda <- model_build_plsda(meta = meta,
                                   species = species,
                                   spectra = spectra,
                                   ncomp = ncomp,
-                                  threads = 1) # If windows = 1
+                                  threads = 2) # If windows = 1
 
 # Based on LDA
 models_lda <- model_build_lda(meta = meta,
@@ -142,7 +158,7 @@ models_lda <- model_build_lda(meta = meta,
                               segments = segments,
                               species = species,
                               spectra = spectra,
-                              threads = 1) # If windows = 1
+                              threads = 2) # If windows = 1
 
 
 #-------------------------------------------------------------------------------
@@ -151,31 +167,34 @@ models_lda <- model_build_lda(meta = meta,
 
 # This return the stats of the model performance and the predicted probabilities
 
-performance_plsda_training <- model_performance_plsda(meta_split = meta[split,],
+performance_plsda_training <- model_performance_plsda(meta_split = meta[split, ],
                                                       species_split = species[split], 
                                                       spectra_split = spectra[split, ],
                                                       models = models_plsda,
                                                       ncomp = ncomp,
-                                                      threads = 1)
+                                                      threads = 2)
 
-performance_plsda_testing <- model_performance_plsda(meta_split = meta[!split,],
-                                                     species_split = species[!split], 
+#generate anti-split numeric vector for species
+anti_split <- setdiff(1:length(species), split)
+
+performance_plsda_testing <- model_performance_plsda(meta_split = meta[!split, ],
+                                                     species_split = species[anti_split], 
                                                      spectra_split = spectra[!split, ],
                                                      models = models_plsda,
                                                      ncomp = ncomp,
-                                                     threads = 1)
+                                                     threads = 2)
 
-performance_lda_training <- model_performance_lda(meta_split = meta[split,],
+performance_lda_training <- model_performance_lda(meta_split = meta[split, ],
                                                   species_split = species[split], 
                                                   spectra_split = spectra[split, ],
                                                   models = models_lda,
-                                                  threads = 1)
+                                                  threads = 2)
 
-performance_lda_testing <- model_performance_lda(meta_split = meta[!split,],
-                                                 species_split = species[!split], 
-                                                 spectra_split = spectra[!split, ],
-                                                 models = models_lda,
-                                                 threads = 1)
+performance_lda_testing <- model_performance_lda(meta_split = meta[!split, ],
+                                                  species_split = species[anti_split], 
+                                                  spectra_split = spectra[!split, ],
+                                                  models = models_lda,
+                                                  threads = 2)
 
 #-------------------------------------------------------------------------------
 #' @Confusion-Matrices
@@ -189,7 +208,7 @@ CM_plsda_training <- confusionMatrices_plsda(meta_split = meta[split,],
                                              threads = 1)
 
 CM_plsda_testing <- confusionMatrices_plsda(meta_split = meta[!split,],
-                                            species_split = species[!split], 
+                                            species_split = species[anti_split], 
                                             spectra_split = spectra[!split, ],
                                             models = models_plsda,
                                             ncomp = ncomp,
@@ -202,7 +221,7 @@ CM_lda_training <- confusionMatrices_lda(meta_split = meta[split,],
                                          threads = 1)
 
 CM_lda_testing <- confusionMatrices_lda(meta_split = meta[!split,],
-                                        species_split = species[!split], 
+                                        species_split = species[anti_split], 
                                         spectra_split = spectra[!split, ],
                                         models = models_lda,
                                         threads = 1)
