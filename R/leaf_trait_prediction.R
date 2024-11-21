775913b8dcfb598097abdb69191e34bd1904c21a
Antguz <antguz06@gmail.com>
2024-11-21 00:18:46 -0500

    Dinamic modelling and errors

--- R/leaf_trait_prediction.R
+++ R/leaf_trait_prediction.R
@@ -35,47 +35,72 @@ source("auxiliary/pls_vip.R")
 #-------------------------------------------------------------------------------
 
 # Select the root_folder to read data and export results
-# Antonio
-#root_path <- "C:/Users/jog4076/Downloads"
-# DW
-root_path <- "/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/herbarium-spectra"
 
+# Antonio
+root_path <- getwd()
 
 #'------------------------------------------------------------------------------
 #' @Read-Information_Reshape
 #-------------------------------------------------------------------------------
 
-# Select the file of interest
+# Define source
+source <- "Kothari" #Kothari or HUH
 
-# HUH
-frame <- fread("/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/dataHUH2024_sp25leaf563_norm_5nm_400-2400.csv")
-frame <- frame[!is.na(leafKg_m2),]
-# HUH data
-meta <- frame[, c("collector", "accession", "accession_leaf", "leaf", "scan", 
-                  "species","genus","family","order","class",
-                  "growthForm", "ddmmyyScanned","doy","absoluteAge", 
-                  "herbQuality","damage", "glue", "leafStage", "greenIndex")]
-meta$sample <- 1:nrow(meta)
-traits <- frame[, c("leafKg_m2", "leafThickness")]
-spectra <- frame[, .SD, .SDcols = 23:ncol(frame)]
+# Define bands of interest
+bands <- seq(450, 2400, by = 5)
 
+# Define out_path of results
+out_path <- paste0(root_path, "/results/", source, "/", source, "_", min(bands), "-", max(bands))
+dir.create(out_path, recursive = TRUE)
 
-# Kothari
-frame <- fread("/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/dataKothari_pressed_unavg_norm_5nm_400-2400.csv")
-meta <- frame[, c("accession", "speciesAuthor", "species", "genus", "LatinEpithet", 
-                  "sp10", "Project", "Discoloration", "Stage", "growthForm", 
-                  "Latitude", "Longitude", "normalization_magnitude", "greenIndex")]
-meta$sample <- 1:nrow(meta)
-traits <- frame[, c("leafKg_m2", "LDMC", "EWT", "EWT_rehydrated", "N", "C", "NDF", 
-                    "ADF", "ADL", "solubles", "hemicellulose", "cellulose", "lignin", 
-                    "chlA", "chlB", "car", "Al", "Ca", "Cu", "Fe", "K", "Mg", "Mn", 
-                    "Na", "P", "Zn", "N_area", "C_area", "solubles_area", 
-                    "hemicellulose_area", "cellulose_area", "lignin_area", 
-                    "chlA_area", "chlB_area", "car_area", "Al_area", "Ca_area", 
-                    "Cu_area", "Fe_area", "K_area", "Mg_area", "Mn_area", "Na_area", 
-                    "P_area", "Zn_area")]
-spectra <- frame[, .SD, .SDcols = 61:ncol(frame)]
-
+if(source == "HUH") {
+  
+  # HUH
+  frame <- fread(paste0(root_path, "/data/dataHUH2024_sp25leaf563_cwt_450-2400.csv"))
+  frame <- frame[!is.na(leafKg_m2),]
+  
+  # HUH meta
+  meta <- frame[, c("collector", "accession", "accession_leaf", "leaf", "scan", 
+                    "species","genus","family","order","class",
+                    "growthForm", "ddmmyyScanned","doy","absoluteAge", 
+                    "herbQuality","damage", "glue", "leafStage", "greenIndex")]
+  meta$sample <- 1:nrow(meta)
+  
+  # HUH traits
+  traits <- frame[, c("leafKg_m2", "leafThickness")]
+  
+  # HUH bands
+  cbands <- as.character(bands)
+  spectra <- frame[, ..cbands]
+  
+} else if(source == "Kothari") {
+  
+  # Kothari
+  frame <- fread(paste0(root_path, "/data/dataKothari_pressed_unavg_cwt_450-2400.csv"))
+  frame <- frame[!is.na(leafKg_m2),]
+  
+  # Kothari meta
+  meta <- frame[, c("name", "accession", "Species", "LatinGenus", "LatinSpecies", 
+                    "Project", "Discoloration", "Stage", "GrowthForm", "Latitude", "Longitude",
+                    "species", "sp10", "greenIndex")]
+  #meta <- meta[sp10 == TRUE,]
+  meta$sample <- 1:nrow(meta)
+  
+  # Kothari traits
+  traits <- frame[, c("leafKg_m2", "LDMC", "EWT", "EWT_rehydrated", "N", "C", "NDF", 
+                      "ADF", "ADL", "solubles", "hemicellulose", "cellulose", "lignin", 
+                      "chlA", "chlB", "car", "Al", "Ca", "Cu", "Fe", "K", "Mg", "Mn", 
+                      "Na", "P", "Zn", "N_area", "C_area", "solubles_area", 
+                      "hemicellulose_area", "cellulose_area", "lignin_area", 
+                      "chlA_area", "chlB_area", "car_area", "Al_area", "Ca_area", 
+                      "Cu_area", "Fe_area", "K_area", "Mg_area", "Mn_area", "Na_area", 
+                      "P_area", "Zn_area")]
+  
+  # Kothari bands
+  cbands <- as.character(bands)
+  spectra <- frame[, ..cbands]
+  
+}
 
 #-------------------------------------------------------------------------------
 #' @Data-split
@@ -85,34 +110,30 @@ spectra <- frame[, .SD, .SDcols = 61:ncol(frame)]
 split <- data_split(meta = meta)
 
 # Export for record
-saveRDS(split, paste0(root_path, "/pls_huh5nm_split.rds"))
-saveRDS(split, paste0(root_path, "/pls_kothari5nm_split.rds"))
+saveRDS(split, paste0(out_path, "/pls_", source, "_split.rds"))
 
 #-------------------------------------------------------------------------------
 #' @Segments
 #-------------------------------------------------------------------------------
 
 # Select a spectral measurement per specimen
-iterations <- 10
+iterations <- 100
 segments <- pbmclapply(X = 1:iterations,
                        FUN = data_segments,
                        meta = meta,
                        split = split,
                        mc.set.seed = TRUE,
-                       mc.cores = 1) # If windows = 1
+                       mc.cores = 25) # If windows = 1
 
 # Export for record
-saveRDS(segments, paste0(root_path, "/pls_huh5nm_segments.rds"))
-saveRDS(segments, paste0(root_path, "/pls_kothari5nm_segments.rds"))
+saveRDS(segments, paste0(out_path, "/pls", source, "_segments.rds"))
 
 #-------------------------------------------------------------------------------
 #' @Model_tune
 #-------------------------------------------------------------------------------
 
 # Select the optimal number of components for all the iterations
-
-# Max number of comp to run, can't run over 24.
-ncomp_max <- 20
+ncomp_max <- 30
 
 # Models to evaluate the optimal
 opt_models <- model_tune(meta = meta,
@@ -121,54 +142,40 @@ opt_models <- model_tune(meta = meta,
                          traits = traits[,1],
                          spectra = spectra,
                          ncomp_max = ncomp_max,
-                         threads = 1) # If windows = 1
-
-#select mode of Ncomp across models (from one-sigma method)
-ncomp <- as.numeric(names(sort(table(opt_models$ncomp), decreasing = TRUE)[1]))
-
-write(ncomp, "ncomp_onesigma-herbarium.txt")
-
-# Calculate optimal number of components
-n_components_PRESS <- which.min(colMeans(opt_models[metric == "PRESS" | estimate == "PRESS", 6:ncol(opt_models)]))
-n_components_RMSEP <- which.min(colMeans(opt_models[metric == "RMSEP" | estimate == "RMSEP", 6:ncol(opt_models)]))
-n_components_R2 <- which.max(colMeans(opt_models[metric == "R2" | estimate == "R2", 6:ncol(opt_models)]));
-
-# Print all values in a single line
-cat(sprintf("Optimal number of components based on max or min values - PRESS: %d, RMSEP: %d, R2: %d\n", 
-            n_components_PRESS, n_components_RMSEP, n_components_R2), 
-    file = "ncomp_otherMetrics-herbarium.txt")
+                         threads = 25) # If windows = 1
 
 # Plot optima
 #PRESS
-pdf("Ncomp_PRESS_plot.pdf", width = 5, height = 4)
-plot(x = 1:(ncol(opt_models)-5),
-     y = colMeans(opt_models[metric == "PRESS" | estimate == "PRESS", 6:ncol(opt_models)]),
+pdf(paste0(out_path, "/Ncomp_PRESS_plot.pdf"), width = 5, height = 4)
+plot(x = 1:30,
+     y = colMeans(opt_models[metric == "PRESS" | estimate == "PRESS", 7:ncol(opt_models)]),
      main = "Optimal number of components",
      xlab = "Number of components",
      ylab = "PRESS")
 dev.off()
 
 #R2
-pdf("Ncomp_R2_plot.pdf", width = 5, height = 4)
-plot(x = 1:(ncol(opt_models)-5),
-     y = colMeans(opt_models[metric == "R2" | estimate == "R2", 6:ncol(opt_models)]),
+pdf(paste0(out_path, "/Ncomp_R2_plot.pdf"), width = 5, height = 4)
+plot(x = 1:30,
+     y = colMeans(opt_models[metric == "R2" | estimate == "R2", 7:ncol(opt_models)]),
      main = "Optimal number of components",
      xlab = "Number of components",
      ylab = "R2")
 dev.off()
 
 #RMSEP
-pdf("Ncomp_RMSEP_plot.pdf", width = 5, height = 4)
-plot(x = 1:(ncol(opt_models)-5),
-     y = colMeans(opt_models[metric == "RMSEP" | estimate == "RMSEP", 6:ncol(opt_models)]),
+pdf(paste0(out_path,"/Ncomp_RMSEP_plot.pdf"), width = 5, height = 4)
+plot(x = 1:30,
+     y = colMeans(opt_models[metric == "RMSEP" | estimate == "RMSEP", 7:ncol(opt_models)]),
      main = "Optimal number of components",
      xlab = "Number of components",
      ylab = "RMSEP")
 dev.off()
 
 # Export csv of statistics for record and figures
-fwrite(opt_models, paste0(root_path, "/pls_huh5nm_opt_comp_models.csv"))
-#fwrite(opt_models, paste0(root_path, "/pls_kothari5nm_opt_comp_models.csv"))
+fwrite(opt_models, paste0(out_path, "/pls_", source, "_opt_comp_models.csv"))
+
+ncomp <- 7
 
 #-------------------------------------------------------------------------------
 #' @Final_model for prediction
@@ -180,69 +187,42 @@ models <- model_build(meta = meta,
                       traits = traits[,1],
                       spectra = spectra,
                       ncomp = ncomp,
-                      threads = 1) # If windows = 1
+                      threads = 25) # If windows = 1
 
 # Export models
-saveRDS(models, paste0(root_path, "/pls_huh5nm_final_models.rds"))
-#saveRDS(models, paste0(root_path, "/pls_kothari5nm_final_models.rds"))
+#saveRDS(models, paste0(out_path, "/pls_", source, "_final_models.rds"))
 
 #-------------------------------------------------------------------------------
 #' @Performance-training
 #-------------------------------------------------------------------------------
 
-# Herbarium
 training_performance <- model_performance(meta_split = meta[split,],
                                           traits_split = traits[split, 1], 
                                           spectra_split = spectra[split,],
                                           models = models,
                                           ncomp = ncomp,
-                                          threads = 1) # If windows = 1
+                                          threads = 25) # If windows = 1
 
-fwrite(training_performance$performance, paste0(root_path, "/pls_huh5nm_training_performance.csv"))
-fwrite(training_performance$predicted, paste0(root_path, "/pls_huh5nm_training_obs-pred.csv"))
-fwrite(training_performance$predicted_by_accession, paste0(root_path, "/pls_huh5nm_training_obs-pred-avg.csv"))
-
-# Pressed
-training_performance_kothari <- model_performance_kothari(meta_split = meta[split,],
-                                                          traits_split = traits[split, 1], 
-                                                          spectra_split = spectra[split,],
-                                                          models = models,
-                                                          ncomp = ncomp,
-                                                          threads = 1) # If windows = 1
-
-# Export for record and figures
-fwrite(training_performance_kothari$performance, paste0(root_path, "/pls_kothari5nm_training_performance.csv"))
-fwrite(training_performance_kothari$predicted, paste0(root_path, "/pls_kothari5nm_training_obs-pred.csv"))
-fwrite(training_performance_kothari$predicted_by_accession, paste0(root_path, "/pls_kothari5nm_training_obs-pred-avg.csv"))
+# Export
+fwrite(training_performance$performance, paste0(out_path, "/pls_", source, "_training_performance.csv"))
+fwrite(training_performance$predicted, paste0(out_path, "/pls_", source, "_training_obs-pred.csv"))
+fwrite(training_performance$predicted_by_accession, paste0(out_path, "/pls_", source, "_training_obs-pred-avg.csv"))
 
 #-------------------------------------------------------------------------------
 #' @Performance-testing
 #-------------------------------------------------------------------------------
 
-# Herbarium
 testing_performance <- model_performance(meta_split = meta[!split,],
                                          traits_split = traits[!split, 1], 
                                          spectra_split = spectra[!split,],
                                          models = models,
                                          ncomp = ncomp,
-                                         threads = 1) # If windows = 1
+                                         threads = 25) # If windows = 1
 
-# write
-fwrite(testing_performance$performance, paste0(root_path, "/pls_huh5nm_testing_performance.csv"))
-fwrite(testing_performance$predicted, paste0(root_path, "/pls_huh5nm_testing_obs-pred.csv"))
-fwrite(testing_performance$predicted_by_accession, paste0(root_path, "/pls_huh5nm_testing_obs-pred-avg.csv"))
-
-#Pressed
-testing_performance_kothari <- model_performance_kothari(meta_split = meta[!split,],
-                                         traits_split = traits[!split, 1], 
-                                         spectra_split = spectra[!split,],
-                                         models = models,
-                                         ncomp = ncomp,
-                                         threads = 1) # If windows = 1
-
-fwrite(testing_performance_kothari$performance, paste0(root_path, "/pls_kothari5nm_testing_performance.csv"))
-fwrite(testing_performance_kothari$predicted, paste0(root_path, "/pls_kothari5nm_testing_obs-pred.csv"))
-fwrite(testing_performance_kothari$predicted_by_accession, paste0(root_path, "/pls_kothari5nm_testing_obs-pred-avg.csv"))
+# Export
+fwrite(testing_performance$performance, paste0(out_path, "/pls_", source, "_testing_performance.csv"))
+fwrite(testing_performance$predicted, paste0(out_path, "/pls_", source, "_testing_obs-pred.csv"))
+fwrite(testing_performance$predicted_by_accession, paste0(out_path, "/pls_", source, "_testing_obs-pred-avg.csv"))
 
 #-------------------------------------------------------------------------------
 #' @Model_coefficients
@@ -251,8 +231,7 @@ fwrite(testing_performance_kothari$predicted_by_accession, paste0(root_path, "/p
 coefficients <- pls_coefficients(models = models,
                                  ncomp = ncomp)
 
-fwrite(coefficients, paste0(root_path, "/pls_huh5nm_coefficients.csv"))
-#fwrite(coefficients, paste0(root_path, "/pls_kothari5nm_coefficients.csv"))
+fwrite(coefficients, paste0(out_path, "/pls_", source, "_coefficients.csv"))
 
 #-------------------------------------------------------------------------------
 #' @Model_VIP
@@ -261,78 +240,7 @@ fwrite(coefficients, paste0(root_path, "/pls_huh5nm_coefficients.csv"))
 vip <- pls_vip(models = models,
                ncomp = ncomp)
 
-fwrite(vip, paste0(root_path, "/pls_huh5nm_vip.csv"))
-#fwrite(vip, paste0(root_path, "/pls_kothari5nm_vip.csv"))
-
-#-------------------------------------------------------------------------------
-#' @Pressed-vs-herbarium-performance
-#-------------------------------------------------------------------------------
-
-# pressMod_herbSpec: pressed models on herbarium spectra
-
-# press models
-pres_models <- readRDS("pls_kothari5nm_final_models.rds")
-
-# load herbarium spectra and metadata 
-frame <- fread("/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/dataHUH2024_sp25leaf563_norm_5nm_400-2400.csv")
-frame <- frame[!is.na(leafKg_m2),]
-# HUH data
-meta <- frame[, c("collector", "accession", "accession_leaf", "leaf", "scan", 
-                  "species","genus","family","order","class",
-                  "growthForm", "ddmmyyScanned","doy","absoluteAge", 
-                  "herbQuality","damage", "glue", "leafStage", "greenIndex")]
-meta$sample <- 1:nrow(meta)
-traits <- frame[, c("leafKg_m2", "leafThickness")]
-spectra <- frame[, .SD, .SDcols = 23:ncol(frame)]
-
-# Run predictions, need ncomp from above
-presMod_herbSpec <- model_performance(meta_split = meta,
-                                         traits_split = traits[,1], 
-                                         spectra_split = spectra,
-                                         models = pres_models,
-                                         ncomp = 9,
-                                         threads = 1) # If windows = 1
-
-fwrite(presMod_herbSpec$performance, paste0(root_path, "/pls_presMod_herbSpec_performance.csv"))
-fwrite(presMod_herbSpec$predicted, paste0(root_path, "/pls_presMod_herbSpec_obs-pred.csv"))
-fwrite(presMod_herbSpec$predicted_by_accession, paste0(root_path, "/pls_presMod_herbSpec_obs-pred-avg.csv"))
-
-
-#-------------------------------------------------------------------------------
-# herbMod_presSpec: herbarium models on pressed spectra
-
-# herb models
-herb_models <- readRDS("pls_huh5nm_final_models.rds")
-
-# press spec
-frame <- fread("/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/dataKothari_pressed_unavg_norm_5nm_400-2400.csv")
-meta <- frame[, c("accession", "speciesAuthor", "species", "genus", "LatinEpithet", 
-                  "sp10", "Project", "Discoloration", "Stage", "growthForm", 
-                  "Latitude", "Longitude", "normalization_magnitude", "greenIndex")]
-meta$sample <- 1:nrow(meta)
-traits <- frame[, c("leafKg_m2", "LDMC", "EWT", "EWT_rehydrated", "N", "C", "NDF", 
-                    "ADF", "ADL", "solubles", "hemicellulose", "cellulose", "lignin", 
-                    "chlA", "chlB", "car", "Al", "Ca", "Cu", "Fe", "K", "Mg", "Mn", 
-                    "Na", "P", "Zn", "N_area", "C_area", "solubles_area", 
-                    "hemicellulose_area", "cellulose_area", "lignin_area", 
-                    "chlA_area", "chlB_area", "car_area", "Al_area", "Ca_area", 
-                    "Cu_area", "Fe_area", "K_area", "Mg_area", "Mn_area", "Na_area", 
-                    "P_area", "Zn_area")]
-spectra <- frame[, .SD, .SDcols = 61:ncol(frame)]
-
-## Run predictions, need ncomp from above
-herbMod_presSpec <- model_performance_kothari(meta_split = meta,
-                                                traits_split = traits[,1], 
-                                                spectra_split = spectra,
-                                                models = herb_models,
-                                                ncomp = 10,
-                                                threads = 1) # If windows = 1
-
-# Write output
-fwrite(herbMod_presSpec$performance, paste0(root_path, "/pls_herbMod_presSpec_performance.csv"))
-fwrite(herbMod_presSpec$predicted, paste0(root_path, "/pls_herbMod_presSpec_obs-pred.csv"))
-fwrite(herbMod_presSpec$predicted_by_accession, paste0(root_path, "/pls_herbMod_presSpec_obs-pred-avg.csv"))
-
+fwrite(vip, paste0(root_path, "/pls_", source, "_vip.csv"))
 
 #-------------------------------------------------------------------------------
 #' @END
---
+++ R/spectra_transform.R
@@ -0,0 +1,122 @@
+#'------------------------------------------------------------------------------
+#' @title Spectral homogenization among sensors
+#'------------------------------------------------------------------------------
+
+#'------------------------------------------------------------------------------
+#' @Library
+#-------------------------------------------------------------------------------
+
+library(data.table)
+library(CWT)
+
+#'------------------------------------------------------------------------------
+#' @Working_directory
+#-------------------------------------------------------------------------------
+
+root_path <- getwd()
+
+#'------------------------------------------------------------------------------
+#' @HUH
+#-------------------------------------------------------------------------------
+
+# Read database
+data <- fread(paste0(root_path, "/data/dataHUH2024_sp25leaf563_noResample_350-2500.csv"))
+
+# Get meta and spectra
+meta <- data[, .SD, .SDcols = 1:22]
+spectra <- data[, .SD, .SDcols = 23:ncol(data)]
+
+# Difference between consecutive bands (resolution)
+bands <- as.numeric(colnames(spectra))
+resolution <- diff(bands)
+range(resolution) # Lower and higher resolution
+res <- 5.0 # Define resolution to use
+
+# Resample spectra
+resampled_bands <- ceiling(seq(340, 2510, res))
+FHWM <- rep(res, length(resampled_bands))
+
+spectra_resampled <- resampling_FWHM(spectra = spectra,
+                                     wavelengths = bands,
+                                     new_wavelengths = resampled_bands,
+                                     FWHM = FHWM)
+
+plot(as.numeric(colnames(spectra_resampled)), 
+     as.matrix(spectra_resampled[50,])[1,])
+
+# Remove edges
+spectra_resampled <- spectra_resampled[, .SD, .SDcols = 3:(ncol(spectra_resampled)-2)]
+
+plot(as.numeric(colnames(spectra_resampled)), 
+     as.matrix(spectra_resampled[50,])[1,])
+
+# Transform spectra
+spectra_transformed <- cwt(t = spectra_resampled, 
+                           scales = c(2, 3, 4), 
+                           variance = 1, 
+                           summed_wavelet = TRUE, 
+                           threads = 1L)
+
+plot(as.numeric(colnames(spectra_transformed)), 
+     as.matrix(spectra_transformed[50,])[1,])
+abline(v = 450)
+abline(v = 2400)
+
+# Remove edges
+spectra_export <- spectra_transformed[, .SD, .SDcols = 21:411]
+
+# Export
+fwrite(cbind(meta, spectra_export), 
+       paste0(root_path, "/data/dataHUH2024_sp25leaf563_cwt_400-2400.csv"))
+
+#'------------------------------------------------------------------------------
+#' @Kothari
+#-------------------------------------------------------------------------------
+
+# Read database
+data <- fread(paste0(root_path, "/data/dataKothari_pressed_unavg_noResample_350-2500.csv"))
+
+# Get meta and spectra
+meta <- data[, .SD, .SDcols = 1:59]
+spectra <- data[, .SD, .SDcols = 60:ncol(data)]
+
+# Difference between consecutive bands (resolution)
+bands <- as.numeric(colnames(spectra))
+resolution <- diff(bands)
+range(resolution) # Lower and higher resolution
+res <- 5 # Define resolution to use
+
+# Resample spectra
+resampled_bands <- ceiling(seq(350, 2500, res))
+FHWM <- rep(res, length(resampled_bands))
+
+spectra_resampled <- resampling_FWHM(spectra = spectra,
+                                     wavelengths = bands,
+                                     new_wavelengths = resampled_bands,
+                                     FWHM = FHWM)
+
+plot(as.numeric(colnames(spectra_resampled)), 
+     as.matrix(spectra_resampled[50,])[1,])
+
+# Remove edges
+spectra_resampled <- spectra_resampled[, .SD, .SDcols = 3:(ncol(spectra_resampled)-2)]
+
+plot(as.numeric(colnames(spectra_resampled)), 
+     as.matrix(spectra_resampled[50,])[1,])
+
+# Transform spectra
+spectra_transformed <- cwt(t = spectra_resampled, 
+                           scales = c(2, 3, 4), 
+                           variance = 1, 
+                           summed_wavelet = TRUE, 
+                           threads = 1L)
+
+plot(as.numeric(colnames(spectra_transformed)), 
+     as.matrix(spectra_transformed[50,])[1,])
+
+# Bands of interest
+spectra_export <- spectra_transformed[, .SD, .SDcols = 19:409]
+
+# Export
+fwrite(cbind(meta, spectra_export), 
+       paste0(root_path, "/data/dataKothari_pressed_unavg_cwt_400-2400.csv"))
