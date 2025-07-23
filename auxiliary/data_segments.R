3f3502bf682c6a017f4fa250d2bbbefcb93bf47d
Dawson White <dawson.white@gmail.com>
2025-07-23 16:24:36 -0400

    Update data_segments.R

--- auxiliary/data_segments.R
+++ auxiliary/data_segments.R
@@ -9,7 +9,7 @@ data_segments <- function(X,
   meta_split <- meta[split,]
   
   # Unique accession
-  unique_accession <- unique(meta_split$accession)
+  unique_accession <- unique(meta_split$specimenIdentifier)
   
   # Collector
   collector <- as.numeric()
@@ -17,7 +17,7 @@ data_segments <- function(X,
   # Loop for selection
   for(i in 1:length(unique_accession)) {
     
-    sub_accession <- subset(meta_split, accession == unique_accession[i])
+    sub_accession <- subset(meta_split, specimenIdentifier == unique_accession[i])
     samp <- sample(sub_accession$sample, 1)
     collector <- c(collector, samp)
     
