2c7ebde9ec8876d3b9ea268f5747f249849f9be3
Dawson White <dawson.white@gmail.com>
2025-07-23 15:38:19 -0400

    Update Spectrolab_process_data.R

--- R/Spectrolab_process_data.R
+++ R/Spectrolab_process_data.R
@@ -13,33 +13,31 @@
 library(readr)
 library(spectrolab)
 library(shiny)
+library(dplyr)
 
 #'------------------------------------------------------------------------------
 #' @Working_directory
 #-------------------------------------------------------------------------------
 
 # set wd
-setwd("~/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/RAnalysesHUHSpectra/herbarium-spectra/")
+setwd("~/Downloads/")
 
 #'------------------------------------------------------------------------------
-#' @Read-Data
+#' @Process-Herbarium-Spectra
 #-------------------------------------------------------------------------------
 
 # Spectral data within directory
-data_refl = spectrolab::read_spectra(path = "~/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/Scan Data Archive/leaf_scans/",extract_metadata = TRUE)
+data_refl = spectrolab::read_spectra(path = "~/Downloads/DMWhiteHUHspec1_raw_spectra_files/",extract_metadata = TRUE)
 
 ####### get filenames
 # Specify the path to the directory containing your spectra files
-data_dir <- "~/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/Scan Data Archive/leaf_scans/"
+data_dir <- "~/Downloads/DMWhiteHUHspec1_raw_spectra_files/"
 
 # List all the files in the directory
 file_list <- list.files(path = data_dir, full.names = TRUE)
 
-#'------------------------------------------------------------------------------
-#' @Process-Spectra-HUH
-#-------------------------------------------------------------------------------
-#######
-# match sensor overlap
+###############################
+#  match sensor overlap
 
 ## Guess "good" splicing bands
 #splice_guess = spectrolab::guess_splice_at(data_refl)
@@ -53,172 +51,41 @@ splice_at = c(991.3, 1902.5)
 # Finally, match the sensor overlap, interpolate_wv=extent around splice_at values over which the splicing factors will be calculated.
 data_refl = spectrolab::match_sensors(x = data_refl, splice_at = splice_at, interpolate_wvl = c(5, 1))
 
-#-------------------------------------------------------------------------------
-#' @Normalize
-#-------------------------------------------------------------------------------
-
+###############################
 # Normalize spectra
+
 data_refl_norm = spectrolab::normalize(data_refl)
 
-#-------------------------------------------------------------------------------
-#' @Match-metadata-and-spectra
-#-------------------------------------------------------------------------------
+###############################
+# Match metadata and spectra
 
-# set spectral data, normalized or otherwise
-spectra <- data_refl_norm
+# set spectral data: data_refl or data_refl_norm
+spectra <- data_refl
+#spectra <- data_refl_norm
 
-# rename spectra
-#names(data_refl) = meta(x = data_refl, label = "name", simplify = TRUE)
-###### redo to get filenames
 # Extract just the base filenames (without directory path and extension)
 names(spectra) <- tools::file_path_sans_ext(basename(file_list))
-######
 
-# Parse names of spectra files to get accession number information
-split_names = strsplit(names(spectra), split = "_", fixed = F) # split spectra filenames to get accession
-accession   = sapply(split_names, `[`, 1)
-leaf        = sapply(split_names, `[`, 2)
-scan <- sapply(strsplit(sapply(split_names, `[`, 3), split = ".", fixed = TRUE), `[`, 1)
-accession_leaf = paste(accession,"_",leaf,sep="")
+# Read metadata spreadsheet
+metadata <- readr::read_csv("~/Downloads/DMWhiteHUHspec1_sp25leaf560_metadata.csv")
 
-# read metadata file
-database <- readr::read_csv("/Users/dawsonwhite/Library/Mobile Documents/com~apple~CloudDocs/Spectroscopy/HUH-Spectra-2024/dataSheet_LMA_metadata-sp25leaf639-1oct2024.csv")
+# Get spectra names (without file extension)
+spectra_names <- names(spectra)
+spectra_base <- tools::file_path_sans_ext(basename(spectra_names))
 
-# get accesssion_leaf number to match rows of database to data_refl
-match_idx = match(accession_leaf, database$catalogNumber_leaf)
+# Match metadata rows using filename (no extension)
+metadata_base <- tools::file_path_sans_ext(metadata$filename)
+match_idx <- match(spectra_base, metadata_base)
 
-# Pull other columns from metadata file:
-collector <- database$Collector[match_idx]
-class <- database$Class[match_idx]
-order <- database$Order[match_idx]
-family <- database$Family[match_idx]
-genus <- database$Genus[match_idx]
-species <- database$species[match_idx]
-ddmmyyScanned <- database$ddmmyyScanned[match_idx]
-absoluteAge <- database$absoluteAge[match_idx]
-herbQuality <- database$herbQuality[match_idx]
-damage <- database$damage[match_idx]
-glue <- database$glue[match_idx]
-leafKg_m2 <- database$leafKg_m2[match_idx]
-leafThickness <- database$leafThickness[match_idx]
-leafStage <- database$leafStage[match_idx]
-hasLMA <- database$hasLMA[match_idx]
-doy <- database$doy[match_idx]
-growthForm <- database$growthForm[match_idx]
+# Reorder metadata to match spectra
+metadata_matched <- metadata[match_idx, ]
 
-#-------------------------------------------------------------------------------
-#' @Build-and-write-data-table
-#-------------------------------------------------------------------------------
+# Combine metadata and spectra into one data frame
+spectra_matrix <- as.data.frame(as.matrix(spectra))  # convert spectra to numeric matrix
+full_data <- cbind(metadata_matched, spectra_matrix)
 
-full_data = data.frame(collector,accession, accession_leaf, leaf, scan, class, order, family, genus, species, growthForm,ddmmyyScanned,doy,absoluteAge,herbQuality,damage,glue,leafKg_m2,leafThickness,leafStage,as.matrix(spectra), check.names = F)
-
-## Add green index
-
-# Calculate the green index for noResample
-greenIndex <- (full_data$`550.5` - full_data$`690.3`) / (full_data$`690.3` + full_data$`550.5`)
-
-# Add the greenIndex as the first column in the dataframe
-full_data <- cbind(greenIndex, full_data)
-
-# Optionally, rename the new column for clarity
-colnames(full_data)[1] <- "greenIndex"
-
-write.csv(full_data, file = "../dataHUH2024_sp25leaf563_norm_350-2500.csv")
-
-# This does not insert a "filename" header in first row. Manually FIX and read in
-
-
-
-
-
-
-
-
-
-#'------------------------------------------------------------------------------
-#' @Kothari-Data
-#-------------------------------------------------------------------------------
-
-#-------------------------------------------------------------------------------
-#' @Load-Spectra-Object
-#-------------------------------------------------------------------------------
-
-pressed <- readRDS("../../Kothari data/pressed_spec_unavg_v3.rds")
-
-#-------------------------------------------------------------------------------
-#' @Splice-overlap-region
-#-------------------------------------------------------------------------------
-
-## Guess "good" splicing bands
-#splice_guess = spectrolab::guess_splice_at(data_refl)
-## Viz the spectra and confirm that the splicing bands make sense
-#spectrolab::plot_interactive(pressed)
-
-## Not doing splicing for these analyses, only removing splice regions.
-# Pick the final splicing bands. Could be the same as `splice_guess`
-#splice_at = c(995)
-# Finally, match the sensor overlap, interpolate_wv=extent around splice_at values over which the splicing factors will be calculated.
-#data_refl = spectrolab::match_sensors(x = pressed, splice_at = splice_at, interpolate_wvl = c(5, 1))
-
-#-------------------------------------------------------------------------------
-#' @Normalize-optional
-#-------------------------------------------------------------------------------
-
-# Normalize spectra
-pressed_norm <- spectrolab::normalize(pressed)
-
-# Assign data
-names_col <- pressed$names
-meta_data <- pressed$meta
-bands <- pressed$bands
-values <- pressed$value
-values_df <- as.data.frame(values)
-colnames(values_df) <- bands
-
-#-------------------------------------------------------------------------------
-#' @Revise-metadata
-#-------------------------------------------------------------------------------
-
-# Format species names to include only the first two words
-meta_data$species <- sapply(strsplit(meta_data$Species, " "), function(x) paste(x[1:min(2, length(x))], collapse = " "))
-
-# Remove rows for specific species
-species_to_remove <- c("Parthenocissus quinquefolia", "Amelanchier humilis")
-filtered_indices <- !meta_data$species %in% species_to_remove
-values_df <- values_df[filtered_indices, ]
-meta_data <- meta_data[filtered_indices, ]
-
-# Add a new column to indicate TRUE/FALSE for PLSDA dataset
-species_vector <- c("Quercus rubra", "Populus tremuloiddes", "Populus grandidentata", 
-                    "Fagus grandifolia", "Betula populifolia", "Betula papyrifera", 
-                    "Agonis flexuosa", "Acer saccharum", "Acer saccharinum", "Acer rubrum")
-meta_data$sp10 <- meta_data$species %in% species_vector
-
-# Green Index= Reflectance550 - Reflectance690 / Reflectance550 + Reflectance690
-
-# Calculate the green index from wavelengths 550 and 690
-greenIndex <- (values_df$`550` - values_df$`690`) / (values_df$`690` + values_df$`550`)
-
-# Add the greenIndex as a new column to the combined data
-combined_data <- cbind(name = names_col[filtered_indices], meta_data, greenIndex, values_df)
-
-# rename ID column as 'accession'
-colnames(combined_data)[colnames(combined_data) == "ID"] <- "accession"
-
-# rename LMA
-colnames(combined_data)[colnames(combined_data) == "LMA"] <- "leafKg_m2"
-
-# rename growthForm
-colnames(combined_data)[colnames(combined_data) == "GrowthForm"] <- "growthForm"
-
-# Write the output to a CSV file
-write.csv(combined_data, file = "../dataKothari_pressed_unavg_noResample_350-2500.csv", row.names = FALSE)
-
-
-
-
-
-
+# Write to CSV
+write.csv(full_data, file = "../DMWhiteHUHspec1_sp25leaf560_noResample_350-2500.csv")
 
 
 #-------------------------------------------------------------------------------
@@ -227,58 +94,58 @@ write.csv(combined_data, file = "../dataKothari_pressed_unavg_noResample_350-250
 
 # Read spectra
 root_path <- getwd()
-frame <- fread(paste0(root_path, "/data/dataHUH2024_sp25leaf563_ref5nm_norm_450-2400.csv"))
+frame <- fread(paste0(root_path, "/DMWhiteHUHspec1_sp25leaf560_noResample_350-2500.csv"))
 
 # Define bands of interest
-bands <- seq(450, 2400, by = 5)
-cbands <- as.character(bands)
+#bands <- seq(450, 2400, by = 5)
+# Identify column index where spectral data starts
+spectral_start <- which(colnames(frame) == "338.6")
 
-# Extract meta and spectra
-meta <- frame[, c("accession", "species", "genus", "family", "growthForm")]
-spectra <- frame[, ..cbands]
+# Subset metadata columns explicitly
+meta <- frame[, c("specimenIdentifier", "scientificName", "Genus", "Family", "growthForm")]
+
+# Subset spectral columns from 338.6 to the end
+spectra <- frame[, spectral_start:ncol(frame)]
+cbands <- as.character(bands)
 
 ## Subset data
 # Define species of interest
 species_of_interest <- "Populus tremuloides"
-# Filter meta
-filtered_sp_meta <- meta[species == species_of_interest]
-# Filter spectra
-filtered_sp_spectra <- spectra[meta$species == species_of_interest]
 
-# Select Accessions of Interest
-# E.g.pick the first 3 unique accessions
-accessions_of_interest <- unique(filtered_sp_meta$accession)[20:29]
-#or make a vector of accession numbers
-#accessions_of_interest <- c("")
-# Filter meta and spectra for the selected accessions
-filtered_meta <- filtered_sp_meta[accession %in% accessions_of_interest]
-filtered_spectra <- filtered_sp_spectra[filtered_sp_meta$accession %in% accessions_of_interest]
+# Filter metadata and spectra by species
+filtered_sp_meta <- meta[meta$scientificName == species_of_interest, ]
+filtered_sp_spectra <- spectra[meta$scientificName == species_of_interest, ]
 
-# Add a sample identifier to differentiate lines
-filtered_meta$sample_id <- seq_len(nrow(filtered_meta))
+# Add a sample identifier column
+filtered_sp_meta$sample_id <- seq_len(nrow(filtered_sp_meta))
 
-# Combine filtered_meta and filtered_spectra
-combined_data <- cbind(filtered_meta, filtered_spectra)
+# Combine metadata and spectra
+combined_data <- cbind(filtered_sp_meta, filtered_sp_spectra)
 
-# Reshape the combined data to long format for plotting
+# Identify spectral column names dynamically
+spectral_cols <- colnames(filtered_sp_spectra)
+
+# Reshape to long format for plotting
+library(tidyr)
 long_data <- combined_data %>%
-  tidyr::pivot_longer(
-    cols = all_of(cbands), # Use column names corresponding to wavelengths
-    names_to = "wavelength",                 # New column for wavelengths
-    values_to = "reflectance"                # New column for reflectance values
+  pivot_longer(
+    cols = all_of(spectral_cols),
+    names_to = "wavelength",
+    values_to = "reflectance"
   )
 
-# Convert wavelength to numeric for correct plotting
+# Convert wavelength to numeric
 long_data$wavelength <- as.numeric(long_data$wavelength)
 
-# Plot the spectra using ggplot
-ggplot(long_data, aes(x = wavelength, y = reflectance, color = accession, group = sample_id)) +
+# Plot
+library(ggplot2)
+ggplot(long_data, aes(x = wavelength, y = reflectance, color = specimenIdentifier, group = sample_id)) +
   geom_line(alpha = 0.7) +
   labs(
-    title = paste("Spectra for", species_of_interest, ":724305 problem"),
+    title = paste("Spectra for", species_of_interest),
     x = "Wavelength (nm)",
     y = "Reflectance",
-    color = "Accession"
+    color = "Specimen"
   ) +
   theme_minimal() +
   theme(
@@ -286,7 +153,8 @@ ggplot(long_data, aes(x = wavelength, y = reflectance, color = accession, group 
     plot.title = element_text(size = 10, face = "bold")
   )
 
-ggsave("spectra_plot_Poptre_problem724305_sm.pdf", plot = last_plot(), device = "pdf", width = 5, height = 3.5)
+# Save the plot
+ggsave("spectra_plot_Poptre_all.pdf", plot = last_plot(), device = "pdf", width = 5, height = 3.5)
 
 
 #-------------------------------------------------------------------------------
